a701507070d8a128fa786293b5fcdb2d3046b124
shaw-31 <anushaisnow@gmail.com>
2017-07-28 00:02:10 +0530

    Update 1-hourglass.py

--- 1-hourglass.py
+++ 1-hourglass.py
@@ -1,51 +1,9 @@
-'''Sample Input
-
-1 1 1 0 0 0
-0 1 0 0 0 0
-1 1 1 0 0 0
-0 0 2 4 4 0
-0 0 0 2 0 0
-0 0 1 2 4 0
-
-Sample Output
-
-19
-
-Explanation
-
-The following hourglasses are:
-
-1 1 1   1 1 0   1 0 0   0 0 0
-  1       0       0       0
-1 1 1   1 1 0   1 0 0   0 0 0
-
-0 1 0   1 0 0   0 0 0   0 0 0
-  1       1       0       0
-0 0 2   0 2 4   2 4 4   4 4 0
-
-1 1 1   1 1 0   1 0 0   0 0 0
-  0       2       4       4
-0 0 0   0 0 2   0 2 0   2 0 0
-
-0 0 2   0 2 4   2 4 4   4 4 0
-  0       0       2       0
-0 0 1   0 1 2   1 2 4   2 4 0
-
-The hourglass with the maximum sum () is:
-
-2 4 4
-  2
-1 2 4
-'''
-
-
 arr=[]
 s=0
 sums=[]
 for i in range(6):
     a1 = [int(x) for x in input().strip().split(' ')]
     arr.append(a1)
-
 for i in range(0,4):
     for j in range(0,4):
         for k in range(j,j+3):
